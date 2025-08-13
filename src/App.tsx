fb17be15fa9e9cc2a4110b41e88b0b78417c3de0
Mostafa Kamel <mkamel30@gmail.com>
2025-08-13 11:51:35 +0300

    [dyad] Fixed a reference error by importing the ReportsPage component. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,20 @@ import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import { Toaster } from "@/components/ui/sonner";
 import { Footer } from "@/components/Footer";
-import { Sidebar } from "@/components/Sidebar"; // Import the new Sidebar component
+import { Sidebar } from "@/components/Sidebar";
+import ReportsPage from "./pages/Reports";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <div className="flex min-h-screen bg-background">
-          <Sidebar /> {/* Add the Sidebar component here */}
-          <main className="flex-1 lg:mr-64 p-4"> {/* Adjust margin for sidebar */}
+          <Sidebar />
+          <main className="flex-1 lg:mr-64 p-4">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/reports" element={<ReportsPage />} /> {/* Ensure ReportsPage is routed */}
+              <Route path="/reports" element={<ReportsPage />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
